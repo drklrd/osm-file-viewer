7dfaad79890af495508b8d503e1816c1c263459e
drklrd <srvbhattarai@gmail.com>
2018-01-07 21:23:55 +0545

    add file reader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 class App extends Component {
+
+       constructor(props){
+           super(props);
+           this.handleChange = this.handleChange.bind(this);
+       }
+
+        handleChange(event){
+            let file = event.target.files[0];
+            console.log(file);
+            let reader  = new FileReader();
+            reader.addEventListener("load", function () {
+                console.log('###',reader.result);
+            }, false);
+            reader.readAsText(file,"UTF-8");
+        }
 
         render() {
             return (
                 <div>
-                    <h1>Hello</h1>
+                    <form>
+                        <input type="file"
+                               name="myFile"
+                               onChange={this.handleChange} />
+                    </form>
                 </div>
             );
         }
